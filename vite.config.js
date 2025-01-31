2149bcfd33da1ce0c4326e044b6bf8ddf40a12e0
denis-cactus <m.play.sys@gmail.com>
2025-01-31 18:04:40 +0200

    Deploy project to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    base: '/goit-js-hw-09',
+    base: '/goit-js-hw-09/',
     root: 'src',
     build: {
       sourcemap: true,
