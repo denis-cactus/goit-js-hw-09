32518443c5891b01493f693d6a0ec61c2b426396
denis-cactus <m.play.sys@gmail.com>
2025-02-01 10:56:52 +0200

    fix formData

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,26 +1,41 @@
-const formData = { email: '', textarea: '' };
 const form = document.querySelector('.feedback-form');
 
+const formData = { email: '', message: '' };
+
 window.addEventListener('load', () => {
+  const savedData = JSON.parse(localStorage.getItem('feedback-form-state')) || {
+    email: '',
+    message: '',
+  };
+  formData.email = savedData.email || '';
+  formData.message = savedData.message || '';
+
   if (formData.email) {
     form.querySelector('[name="email"]').value = formData.email;
   }
-  if (formData.textarea) {
-    form.querySelector('[name="textarea"]').value = formData.textarea;
+  if (formData.message) {
+    form.querySelector('[name="message"]').value = formData.message;
   }
 });
+
 form.addEventListener('input', evt => {
-  const fieldName = evt.target.value;
+  const fieldName = evt.target.name;
   formData[fieldName] = evt.target.value.trim();
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 });
+
 form.addEventListener('submit', evt => {
   evt.preventDefault();
-  if (!formData.email || !formData.textarea) {
+
+  if (!formData.email || !formData.message) {
     alert('Fill please all fields');
-  } else {
-    console.log(formData);
-    localStorage.removeItem('feedback-form-state');
-    form.reset();
+    return;
   }
+
+  console.log('Form data:', formData);
+
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+  formData.email = '';
+  formData.message = '';
 });
